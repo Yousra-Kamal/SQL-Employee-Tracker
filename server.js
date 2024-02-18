0ad2183446c6dbfcbd610e3ae6299fa7e89689f1
Yousra <nice.sora@hotmail.com>
2024-02-18 19:53:21 +1100

    added colors to the console/ added addDepartment function

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 //console.table call once somewhere in the beginning of the app
 const cTable = require("console.table");
+const yellow = "\x1b[33m";
+const blue = "\x1b[34m";
+
+const reset = "\x1b[0m";
 
 // Connect to database
 const db = mysql.createConnection(
@@ -82,6 +86,7 @@ function questions() {
 // View Departments
 function viewDepartments() {
   const sql = `SELECT department.id, department.name AS Department FROM department;`;
+  console.log(yellow + `\n Here is a list of all the Departments: \n` + reset);
   db.query(sql, (err, res) => {
     err ? console.error(err) : console.table(res);
     questions();
@@ -91,6 +96,7 @@ function viewDepartments() {
 // View Roles
 function viewRoles() {
   const sql = `SELECT roles.id, roles.title AS role, roles.salary, department.name AS department FROM roles INNER JOIN department ON (department.id = roles.department_id);`;
+  console.log(yellow + `\n Here is a list of all the Roles: \n` + reset);
   db.query(sql, (err, res) => {
     err ? console.error(err) : console.table(res);
     questions();
@@ -100,8 +106,38 @@ function viewRoles() {
 // View Employees
 function viewEmployees() {
   const sql = `SELECT employee.id, employee.first_name, employee.last_name, roles.title AS role, department.name AS department, roles.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN employee manager on manager.id = employee.manager_id INNER JOIN roles ON (roles.id = employee.role_id) INNER JOIN department ON (department.id = roles.department_id) ORDER BY employee.id;`;
+
+  console.log(yellow + `\n Here is a list of all the Employees: \n` + reset);
   db.query(sql, (err, res) => {
     err ? console.error(err) : console.table(res);
     questions();
   });
+}
+
+// Add Departments
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "department",
+        message: "What is the name of the department?",
+      },
+    ])
+    .then((answer) => {
+      const sql = `INSERT INTO department(name) VALUES('${answer.department}');`;
+      db.query(sql, (err, res) => {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        console.log(
+          yellow + `\n Added ${answer.department} to the database.\n` + reset
+        );
+        db.query(`SELECT * FROM department`, (err, results) => {
+          err ? console.error(err) : console.table(results);
+          questions();
+        });
+      });
+    });
 }
